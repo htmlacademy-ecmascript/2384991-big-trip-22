11ce7375c0c5387dabfc891c2dd28d087b5ae772
Kseniia <inanast153@gmail.com>
2023-12-10 17:13:05 +0100

    Переименует переменную

--- src/main.js
+++ src/main.js
@@ -5,10 +5,10 @@ import { render, RenderPosition } from './render.js';
 
 const siteHeader = document.querySelector('.page-header');
 const tripEvents = document.querySelector('.trip-events');
-const pageBodyContainer = siteHeader.querySelector('.trip-main');
+const tripMain = siteHeader.querySelector('.trip-main');
 const tripFilters = siteHeader.querySelector('.trip-controls__filters');
 const boardPresenter = new BoardPresenter({container: tripEvents});
 
-render(new InfoView(), pageBodyContainer, RenderPosition.AFTERBEGIN);
+render(new InfoView(), tripMain, RenderPosition.AFTERBEGIN);
 render(new FilterView(), tripFilters);
 boardPresenter.init();
