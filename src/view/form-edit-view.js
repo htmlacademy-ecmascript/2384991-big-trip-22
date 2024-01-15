a0d290d7e32ac256411f50c5f1c89d3d4b9bf660
Kseniia <inanast153@gmail.com>
2024-01-15 18:06:53 +0100

    Исправляет разметку формы редактирования

--- src/view/form-edit-view.js
+++ src/view/form-edit-view.js
@@ -34,7 +34,8 @@ const createFormEditView = (point, destination, offersByType, checkedOffers) => 
   const isOffersExist = offersByType && offers && offers.length > 0;
   const offersTemplate = isOffersExist ? createOffersTemplate(offersByType.offers, checkedOffers) : '';
 
-  return (`<form class="event event--edit" action="#" method="post">
+  return (`<li class="trip-events__item">
+  <form class="event event--edit" action="#" method="post">
 <header class="event__header">
   <div class="event__type-wrapper">
     <label class="event__type  event__type-btn" for="event-type-toggle-${offers.id}">
@@ -95,7 +96,8 @@ ${isOffersExist ? `
     </div>
   </section>
 </section>
-</form>`);
+</form>
+</li>`);
 };
 
 export default class FormEditView extends AbstractView {
